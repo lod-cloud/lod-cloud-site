531a803a6890b48d7619a37415f49857a936a109
John P. McCrae <john@mccr.ae>
2024-11-25 10:05:57 +0000

    Update table on index page

--- src/main/java/org/insightcentre/lodcloud/UpdateClouds.java
+++ src/main/java/org/insightcentre/lodcloud/UpdateClouds.java
@@ -51,12 +51,11 @@ public class UpdateClouds extends HttpServlet {
         } else if(line.contains("--TABLE--")) {
           String toInsert = "<tr typeof=\"dctype:Image\" about=\"#cloud\" property=\"dc:title\" content=\"LOD cloud diagram\">\n"
             + "<th property=\"dc:modified\" datatype=\"xsd:date\" content=\"" + date + "\">" + date + "</th>\n" 
-            + "<td></td><td></td><td></td><td>\n"
+            + "<td></td>\n"
             + "<a href=\"versions/" + date + "/lod-cloud.png\">png</a></td>\n" 
-            + "<td></td>\n" 
             + "<td><a href=\"versions/" + date + "/lod-cloud.svg\">svg</a></td>\n"
             + "<td><a href=\"versions/" + date + "/lod-data.json\">json</a></td>\n" 
-            + "<td></td><td class=\"dataset-count\">" + dataset + "</td></tr>\n";
+            + "<td class=\"dataset-count\">" + dataset + "</td></tr>\n";
 
           newTemplate.append(line);
           newTemplate.append("\n");
